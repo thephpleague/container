102f8741f76fd4d77e17b4b050e4d0115df721f1
Phil Bennett <phil@mediabowl.co.uk>
2015-01-13 15:37:25 +0000

    Combined Callable and Closure definitions as they do the same job. Resolves #5

--- src/Definition/ClosureDefinition.php
+++ src/Definition/ClosureDefinition.php
@@ -2,35 +2,10 @@
 
 namespace League\Container\Definition;
 
-use League\Container\ContainerInterface;
-use League\Container\Exception;
-
-class ClosureDefinition extends AbstractDefinition implements DefinitionInterface
+/**
+ * @deprecated will be removed in favour of `CallableDefinition`
+ */
+class ClosureDefinition extends CallableDefinition
 {
-    /**
-     * @var \Closure
-     */
-    protected $closure;
 
-    /**
-     * Constructor
-     *
-     * @param string                      $alias
-     * @param \Closure                    $closure
-     * @param \League\Container\ContainerInterface $container
-     */
-    public function __construct($alias, \Closure $closure, ContainerInterface $container)
-    {
-        parent::__construct($alias, $container);
-
-        $this->closure = $closure;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function __invoke(array $args = [])
-    {
-        return call_user_func_array($this->closure, $this->resolveArguments($args));
-    }
 }
