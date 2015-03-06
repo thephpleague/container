e5f60f4d02546d3e6bdf08d824895e0227784e24
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-03-06 16:27:06 +0100

    Introduce new InvokableDefinition which implements CallableDefIface

--- src/Definition/Factory.php
+++ src/Definition/Factory.php
@@ -11,8 +11,12 @@ class Factory implements FactoryInterface
      */
     public function __invoke($alias, $concrete, ContainerInterface $container, $callable = false)
     {
-        if ($concrete instanceof \Closure || $callable === true) {
-            return new CallableDefinition($alias, $concrete, $container);
+        if (is_callable($concrete)) {
+            if ($callable) {
+                return new InvokableDefinition($alias, $concrete, $container);
+            } else {
+                return new CallableDefinition($alias, $concrete, $container);
+            }
         }
 
         if (is_string($concrete) && class_exists($concrete)) {
---
+++ src/Definition/InvokableDefinition.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace League\Container\Definition;
+
+use League\Container\ContainerAwareInterface;
+use League\Container\ContainerAwareTrait;
+use League\Container\ContainerInterface;
+use ReflectionFunction;
+use ReflectionMethod;
+use ReflectionParameter;
+
+class InvokableDefinition extends AbstractDefinition implements
+    CallableDefinitionInterface,
+    DefinitionInterface,
+    ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @var callable
+     */
+    protected $callable;
+
+    /**
+     * @var array
+     */
+    protected $argumentNames;
+
+    /**
+     * Constructor
+     *
+     * @param string                               $alias
+     * @param string|callable                      $concrete
+     * @param \League\Container\ContainerInterface $container
+     */
+    public function __construct($alias, $concrete, ContainerInterface $container = null)
+    {
+        parent::__construct($alias, $container);
+
+        $this->callable = $concrete;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(array $args = [])
+    {
+        if (is_array($this->callable) && is_string($this->callable[0])) {
+            $registered = (
+                isset($this->container[$this->callable[0]]) ||
+                class_exists($this->callable[0])
+            );
+
+            $this->callable[0] = ($registered) ? $this->container->get($this->callable[0]) : $this->callable[0];
+        }
+
+        $resolved = $this->resolveArguments($this->arguments);
+
+        if ($args) {
+            if (is_null($this->argumentNames)) {
+                $this->argumentNames = $this->getArgumentNames($this->callable);
+            }
+
+            $resolved = array_combine(array_splice($this->argumentNames, 0, count($resolved)), $resolved);
+
+            return $this->container->call($this->callable, array_merge($resolved, $args));
+        }
+
+        return call_user_func_array($this->callable, $resolved);
+    }
+
+    /**
+     * @param callable $callable
+     *
+     * @return array
+     */
+    protected function getArgumentNames(callable $callable)
+    {
+        if (is_array($callable)) {
+            $reflector = new ReflectionMethod($callable[0], $callable[1]);
+        } else {
+            $reflector = new ReflectionFunction($callable);
+        }
+
+        return array_map(function (ReflectionParameter $param) {
+            return $param->getName();
+        }, $reflector->getParameters());
+    }
+}
