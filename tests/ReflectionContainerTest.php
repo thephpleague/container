e9538d50a53eeffabd3c515c2be0710969da01c7
Andreas Möller <am@localheinz.com>
2015-08-30 01:45:01 +0200

    Fix: Assert that ReflectionContainer checks if class exists

---
+++ tests/ReflectionContainerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace League\Container\Test;
+
+use League\Container\ReflectionContainer;
+
+class ReflectionContainerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Asserts that ReflectionContainer claims it has an item if a class exists for the alias.
+     */
+    public function testHasReturnsTrueIfClassExists()
+    {
+        $container = new ReflectionContainer();
+
+        $this->assertTrue($container->has('League\Container\Test\Asset\Bar'));
+    }
+
+    /**
+     * Asserts that ReflectionContainer denies it has an item if a class does not exist for the alias.
+     */
+    public function testHasReturnsFalseIfClassDoesNotExist()
+    {
+        $container = new ReflectionContainer();
+
+        $this->assertFalse($container->has('Foo\Bar\Baz'));
+    }
+}
