c71e5d92070676407f921f970052d75657f50fb0
Andreas Möller <am@localheinz.com>
2015-08-30 02:15:09 +0200

    Fix: Assert that ReflectionContainer instantiates class w/ constructor

--- tests/ReflectionContainerTest.php
+++ tests/ReflectionContainerTest.php
@@ -2,6 +2,7 @@
 
 namespace League\Container\Test;
 
+use League\Container\ImmutableContainerInterface;
 use League\Container\ReflectionContainer;
 
 class ReflectionContainerTest extends \PHPUnit_Framework_TestCase
@@ -36,5 +37,92 @@ class ReflectionContainerTest extends \PHPUnit_Framework_TestCase
         $container = new ReflectionContainer();
 
         $this->assertInstanceOf($classWithoutConstructor, $container->get($classWithoutConstructor));
+    }
+
+    /**
+     * Asserts that ReflectionContainer instantiates a class that has a constructor.
+     */
+    public function testGetInstantiatesClassWithConstructor()
+    {
+        $classWithConstructor = 'League\Container\Test\Asset\Foo';
+        $dependencyClass = 'League\Container\Test\Asset\Bar';
+
+        $container = new ReflectionContainer();
+
+        $item = $container->get($classWithConstructor);
+
+        $this->assertInstanceOf($classWithConstructor, $item);
+        $this->assertInstanceOf($dependencyClass, $item->bar);
+    }
+
+    /**
+     * Asserts that ReflectionContainer instantiates a class that has a constructor with a type-hinted argument, and
+     * fetches that dependency from the container injected into the ReflectionContainer.
+     */
+    public function testGetInstantiatesClassWithConstructorAndUsesContainer()
+    {
+        $classWithConstructor = 'League\Container\Test\Asset\Foo';
+        $dependencyClass = 'League\Container\Test\Asset\Bar';
+        $dependency = new $dependencyClass;
+
+        $container = new ReflectionContainer();
+
+        $container->setContainer($this->getImmutableContainerMock([
+            $dependencyClass => $dependency,
+        ]));
+
+        $item = $container->get($classWithConstructor);
+
+        $this->assertInstanceOf($classWithConstructor, $item);
+        $this->assertSame($dependency, $item->bar);
+    }
+
+    /**
+     * Asserts that ReflectionContainer instantiates a class that has a constructor with a type-hinted argument, and
+     * uses the values provided in the argument array.
+     */
+    public function testGetInstantiatesClassWithConstructorAndUsesArguments()
+    {
+        $classWithConstructor = 'League\Container\Test\Asset\Foo';
+        $dependencyClass = 'League\Container\Test\Asset\Bar';
+        $dependency = new $dependencyClass;
+
+        $container = new ReflectionContainer();
+
+        $item = $container->get($classWithConstructor, [
+            'bar' => $dependency,
+        ]);
+
+        $this->assertInstanceOf($classWithConstructor, $item);
+        $this->assertSame($dependency, $item->bar);
+    }
+
+    /**
+     * @param array $items
+     * @return \PHPUnit_Framework_MockObject_MockObject|ImmutableContainerInterface
+     */
+    private function getImmutableContainerMock(array $items = [])
+    {
+        $container = $this->getMockBuilder('League\Container\ImmutableContainerInterface')->getMock();
+
+        $container
+            ->expects($this->any())
+            ->method('has')
+            ->willReturnCallback(function ($alias) use ($items) {
+                return array_key_exists($alias, $items);
+            })
+        ;
+
+        $container
+            ->expects($this->any())
+            ->method('get')
+            ->willReturnCallback(function ($alias) use ($items) {
+                if (array_key_exists($alias, $items)) {
+                    return $items[$alias];
+                }
+            })
+        ;
+
+        return $container;
     }
 }
