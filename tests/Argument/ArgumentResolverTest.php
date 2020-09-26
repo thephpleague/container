e7b40d77e2d79c3a4ce3172133fb6c9871e26359
Phil Bennett <mail@philbennett.co.uk>
2020-09-26 11:40:51 +0100

    Fix test for multiple versions

--- tests/Argument/ArgumentResolverTest.php
+++ tests/Argument/ArgumentResolverTest.php
@@ -69,12 +69,14 @@ class ArgumentResolverTest extends TestCase
         $param1    = $this->getMockBuilder(ReflectionParameter::class)->disableOriginalConstructor()->getMock();
         $param2    = $this->getMockBuilder(ReflectionParameter::class)->disableOriginalConstructor()->getMock();
         $param3    = $this->getMockBuilder(ReflectionParameter::class)->disableOriginalConstructor()->getMock();
-        $class     = $this->getMockBuilder(ReflectionType::class)->disableOriginalConstructor()->getMock();
+        $class     = $this->getMockBuilder(ReflectionType::class)->disableOriginalConstructor();
         $container = $this->getMockBuilder(Container::class)->getMock();
 
-        if (PHP_VERSION_ID >= 70400) {
+        if (PHP_VERSION_ID >= 70200) {
+            $class = $class->addMethods(['getName'])->getMock();
             $class->expects($this->once())->method('getName')->willReturn('Class');
         } else {
+            $class = $class->getMock();
             $class->expects($this->once())->method('__toString')->willReturn('Class');
         }
 
