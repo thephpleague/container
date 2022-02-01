a68b3e62d49f1a76478651edc822009beac2604e
m1ke <git@m1ke.me>
2022-02-01 11:14:25 +0000

    Implementation that resolves the intended case and causes both new tests to pass

--- src/Definition/Util.php
+++ src/Definition/Util.php
@@ -8,6 +8,10 @@ class Util
 {
     public static function normalizeAlias(string $alias): string
     {
+        if (strpos($alias, '\\') === 0) {
+            return substr($alias, 1);
+        }
+
         return $alias;
     }
 }
