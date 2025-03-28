d6bfd2f606d69502f4916e2fe923a1c673b93c3b
Michiel Devriese <michieldevriese@gmail.com>
2025-03-28 08:26:13 +0100

    Remove `never` return type.
    The `boot` method is not meant to `never` return. It is expected that the boot method, as stated in the documentation, provides the ability to eager load services.

--- src/ServiceProvider/BootableServiceProviderInterface.php
+++ src/ServiceProvider/BootableServiceProviderInterface.php
@@ -9,8 +9,6 @@ interface BootableServiceProviderInterface extends ServiceProviderInterface
     /**
      * Method will be invoked on registration of a service provider implementing
      * this interface. Provides ability for eager loading of Service Providers.
-     *
-     * @return never
      */
     public function boot(): void;
 }
